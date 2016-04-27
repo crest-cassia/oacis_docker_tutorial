ad4599d278566c52e23cd0f76b4e7830caffa75c
takeshi-uchitane <t.uchitane@gmail.com>
2016-04-27 19:15:25 +0900

    install R DoE.base package

--- tutorial/docker_build/R/install-packages.R
+++ tutorial/docker_build/R/install-packages.R
@@ -1,3 +1,3 @@
 dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
-install.packages("corrplot", Sys.getenv("R_LIBS_USER"), repos = "http://cran.ism.ac.jp" )
+install.packages("DoE.base", Sys.getenv("R_LIBS_USER"), repos = "http://cran.ism.ac.jp" )
 
