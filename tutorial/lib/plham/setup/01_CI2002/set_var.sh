d3cd3d3140c2dc9c67f42c7da7f818c72f51621f
takeshi-uchitane <t.uchitane@gmail.com>
2016-05-18 15:11:54 +0900

    rename Plham_CI2002

--- tutorial/lib/plham/setup/01_CI2002/set_var.sh
+++ tutorial/lib/plham/setup/01_CI2002/set_var.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 plham_home=/home/oacis/plham
-tutorial_name="01_CI2002Main"
+tutorial_name="CI2002"
 source_path=$plham_home/samples/CI2002/CI2002Main.x10
 config_path=$plham_home/samples/CI2002/config.json
 binary_path=~/bin/plham/bin/${tutorial_name}.out
