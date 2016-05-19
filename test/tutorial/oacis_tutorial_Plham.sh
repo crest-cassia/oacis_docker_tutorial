75b6099858632d34d2290070d62c32b7bafa7d00
takeshi-uchitane <t.uchitane@gmail.com>
2016-05-19 18:34:12 +0900

    test for plham is updated

--- test/tutorial/oacis_tutorial_Plham.sh
+++ test/tutorial/oacis_tutorial_Plham.sh
@@ -7,7 +7,7 @@ function setup_tutorial_Plham() {
   sleep 180
   test `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "test -f /home/oacis/tutorial/lib/plham/oacis_tutorial_Plham.sh"; echo $?` -eq 0 -a \
     `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "test -d /home/oacis/plham"; echo $?` -eq 0 -a \
-    `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "test -d /home/oacis/oacis/public/Result_development/work/plham"; echo $?` -eq 0 -a \
+    `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "test -f /home/oacis/oacis/public/Result_development/work/plham"; echo $?` -eq 0 -a \
     `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "/home/oacis/plham/samples/CI2002/oacis/run.sh ~/bin/plham/bin/CI2002.out 1.0 0.0 1.0 12345" > /dev/null 2>&1; echo $?` -eq 0 -a \
     `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "mongo --eval \"db = db.getSiblingDB('oacis_development'); db.simulators.find({\\\\\"name\\\\\":\\\\\"Plham_CI2002\\\\\"}).map (function(u) {return u.name;})[0]\"" | tail -1 | tr -d '\r'` == 'Plham_CI2002' -a \
     `docker exec -it ${OACIS_CONTAINER_NAME} bash -c "mongo --eval \"db = db.getSiblingDB('oacis_development'); db.analyzers.find({\\\\\"name\\\\\":\\\\\"Timeseries_Plot\\\\\", \\\\\"simulator_id\\\\\": db.simulators.findOne({\\\\\"name\\\\\":\\\\\"Plham_CI2002\\\\\"})._id}).map (function(u) {return u.name;})[0]\"" | tail -1 | tr -d '\r'` == 'Timeseries_Plot'
